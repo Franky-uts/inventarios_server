5452d190fa5b0cc3d23ea89b1bd7cb6972197ce1
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2025-10-13 17:02:14 -0600

    Actualización de Querys

--- src/controladores/almacen.js
+++ src/controladores/almacen.js
@@ -2,20 +2,32 @@ import {pool} from '../db.js'
 
 export const getAlmacen = async (req,res)=>{
     const {filtro} = req.params
-    const {rows} = await pool.query(`SELECT * FROM almacen ORDER BY "${filtro}";`);
+    const {rows} = await pool.query(`SELECT "Almacen"."id", "Almacen"."Nombre", "Tipos"."Nombre" as "Tipo",
+        "Unidades", "CantidadPorUnidad", "Areas"."Nombre" as "Area", "Entrada", "Salida", "Perdida", to_char("UltimaModificación", 'HH24:MI DD/MM/YYYY')as "UltimaModificación"
+        FROM public."Almacen"
+        INNER JOIN "Tipos" ON "Almacen"."Tipo" = "Tipos"."id"
+	    INNER JOIN "Areas" ON "Almacen"."Area" = "Areas"."id" 
+         ORDER BY "${filtro}";`);
     res.send(rows)
 }
 
 export const getAlmacenBusqueda = async (req,res)=>{
     const {filtro} = req.params
     const {busqueda} = req.params
-    const {rows} = await pool.query(`SELECT * FROM almacen WHERE "Nombre"||"Tipo"||"Area" ILIKE '%${busqueda}%' ORDER BY "${filtro}";`);
+    const {rows} = await pool.query(`SELECT "Almacen"."id", "Almacen"."Nombre", "Tipos"."Nombre" as "Tipo",
+        "Unidades", "CantidadPorUnidad", "Areas"."Nombre" as "Area", "Entrada", "Salida", "Perdida", to_char("UltimaModificación", 'HH24:MI DD/MM/YYYY')as "UltimaModificación"
+        FROM public."Almacen" 
+        INNER JOIN "Tipos" ON "Almacen"."Tipo" = "Tipos"."id"
+	    INNER JOIN "Areas" ON "Almacen"."Area" = "Areas"."id" 
+        WHERE "Almacen"."Nombre"||"Tipos"."Nombre"||"Areas"."Nombre" ILIKE '%${busqueda}%' ORDER BY "${filtro}";`);
     res.send(rows)
 }
 
 export const añadirAlmacen = async (req,res)=>{
     const datos = req.body
-    const {rows} = await pool.query(`INSERT INTO almacen("Nombre", "Tipo", "Unidades", "CantidadPorUnidad", "Area", "Entrada", "Salida", "Perdida", "UltimaModificación") VALUES ('${datos.nombre}', '${datos.tipo}', 0, 0, '${datos.area}', 0, 0, 0, Now()) RETURNING *;`);
+    const {rows} = await pool.query(`INSERT INTO public."Almacen"(
+	"Nombre", "Unidades", "CantidadPorUnidad", "Entrada", "Salida", "Perdida", "UltimaModificación", "Tipo", "Area") 
+    VALUES ('${datos.nombre}', 0, 0, 0, 0, 0, Now(), ${datos.tipo}, '${datos.area}') RETURNING *;`);
     res.send(rows)
 }
 
@@ -29,7 +41,7 @@ export const editarAlmacen = async (req,res)=>{
     const {id} = req.params
     const {columna} = req.params
     const datos = req.body;
-    const {rows} = await pool.query(`UPDATE almacen SET "${columna}" = ${datos.dato} WHERE id = ${id} RETURNING *;`)
+    const {rows} = await pool.query(`UPDATE public."Almacen" SET "${columna}" = ${datos.dato} WHERE id = ${id} RETURNING *;`)
     res.send(rows)
 }
 
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use((req,res,next)=>{
     res.status(404).json({error:"No se encontro "+req.path})
 })
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 4000;
 
 
 app.listen(port,()=>{
