51b580865dd3b32e71f63720652585c9907d6f35
Franky <fran.roac02@gmail.com>
2025-11-03 15:20:36 -0600

    Base de datos en la nube

--- src/db.js
+++ src/db.js
@@ -1,9 +1,18 @@
 import pg from 'pg'
 
 export const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
+
+/*import pg from 'pg'
+
+export const pool = new pg.Pool({
     user: "postgres",
     host: "127.0.0.1",
     password: "Holamundo123",
     database: "postgres",
     port: "5432"
-})
+})*/
