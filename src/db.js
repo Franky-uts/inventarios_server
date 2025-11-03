c6479220aed0af03b3fb98c01572039a43772985
Franky <fran.roac02@gmail.com>
2025-11-03 16:29:36 -0600

    Quite variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import pg from 'pg'
 
 export const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: "postgresql://neondb_owner:npg_sJM87XtBWDiu@ep-twilight-shadow-a43i11yb-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
     ssl: {
         rejectUnauthorized: false,
     },
