194badc644f4824a381b3ba4222db3d673f70f2c
Franky <fran.roac02@gmail.com>
2025-11-03 16:22:20 -0600

    Otro update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send("Servidor de inventarios"+process.env.DATABASE_URL)
+    res.send("Servidor de inventarios")
 })
 
 app.use(express.json())
