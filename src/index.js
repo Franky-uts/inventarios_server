aadbf7496558e9a46626b8e7e5db8002a8cd59c3
Franky <fran.roac02@gmail.com>
2025-11-03 16:34:59 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send("Servidor de inventarios")
+    res.send("Servidor de inventarios"+PORT)
 })
 
 app.use(express.json())
