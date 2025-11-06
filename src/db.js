d4d3e831cae097827911d195c0db034bf2c0bede
Franky <fran.roac02@gmail.com>
2025-11-06 13:34:08 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,18 +1,30 @@
-import pg from 'pg'
+/*import pg from 'pg'
 
 export const pool = new pg.Pool({
     connectionString: process.env.POSTGRES_URL, //"postgresql://neondb_owner:npg_sJM87XtBWDiu@ep-twilight-shadow-a43i11yb-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
     ssl: {
         rejectUnauthorized: false,
     },
-})
+})*/
 
+//Base para Localhost
 /*import pg from 'pg'
 
 export const pool = new pg.Pool({
     user: "postgres",
-    host: "127.0.0.1",
+    host: "localhost",
     password: "Holamundo123",
     database: "postgres",
     port: "5432"
 })*/
+
+//Base para Docker
+import pg from 'pg'
+
+export const pool = new pg.Pool({
+    user: "postgres",
+    host: "db",
+    password: "Holamundo123",
+    database: "postgres",
+    port: "5432"
+})
