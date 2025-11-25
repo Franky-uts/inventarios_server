e27abf16f29c674a582f7199542efc93a2ad5529
Franky <fran.roac02@gmail.com>
2025-11-25 10:35:32 -0600

    Te maldigo CORS

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import ordenes from './rutas/ordenes.js'
 const app = express();
 
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
 
